17cccd89ad3d94dcdf413d773b4e4d97e03ee6fd
ankush adhikari <ankushadhikari321@gmail.com>
2025-10-23 01:32:53 +0530

    api updated

--- frontend/src/lib/AxiosInstances.jsx
+++ frontend/src/lib/AxiosInstances.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosinstance=axios.create({
-    baseURL:"https://video-calling-app-10pu.onrender.com",
+    baseURL:"https://video-calling-app-10pu.onrender.com/api",
     withCredentials:true
 })

50c08457a7e0aa705f3b5e9dac069d09e18a9b34
ankush adhikari <ankushadhikari321@gmail.com>
2025-10-23 00:15:34 +0530

    done backend deployed

--- frontend/src/lib/AxiosInstances.jsx
+++ frontend/src/lib/AxiosInstances.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosinstance=axios.create({
-    baseURL:"http://localhost:5000/api",
+    baseURL:"https://video-calling-app-10pu.onrender.com/",
     withCredentials:true
 })
